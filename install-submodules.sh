e5bef917c2acc3855a00e3280767e4480733b09d
kagamma <kagamma.km@gmail.com>
2022-02-27 10:47:26 +0700

    Adds CMUSphinx headers

--- install-submodules.sh
+++ install-submodules.sh
@@ -2,6 +2,7 @@ rm -rf src/components/castle-engine
 rm -rf src/components/neural-api
 rm -rf src/components/typing-label
 rm -rf src/components/synalist
+rm -rf src/components/TPocketSphinx
 
 cd src/components
 git clone --depth 1 https://github.com/castle-engine/castle-engine.git
@@ -14,5 +15,6 @@ git apply ../patches/neural-api.patch
 cd ..
 git clone https://gitlab.com/EugeneLoza/typing-label.git
 svn checkout https://svn.code.sf.net/p/synalist/code/trunk synalist --trust-server-cert
+git clone --depth 1 https://github.com/r1me/TPocketSphinx.git
 cd ..
 
