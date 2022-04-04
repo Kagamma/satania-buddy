257ea351f08e3237d48d4261bfd3e358c357ac1d
kagamma <kagamma.km@gmail.com>
2022-04-04 17:38:34 +0700

    Adds buffer_Xxx functions

--- src/satania/mcdowell.pas
+++ src/satania/mcdowell.pas
@@ -130,8 +130,8 @@ begin
   Script := TEvilC.Create;
   AnimTalkLoop := 'talk_loop';   
   AnimTalkFinish := 'talk_finish';            
-  Script.RegisterFunc('string_length', @SEStringLength, 1);
-  Script.RegisterFunc('string_at', @SEStringAt, 2);            
+  Script.RegisterFunc('buffer_length', @SEBufferLength, 1);
+  Script.RegisterFunc('buffer_get', @SEBufferGet, 2);
   Script.RegisterFunc('string_insert', @SEStringInsert, 3);
   Script.RegisterFunc('string_delete', @SEStringDelete, 3);
   Script.RegisterFunc('numbers', @SENumbers, 1);     
