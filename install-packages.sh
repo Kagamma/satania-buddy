18b6645141be60b3f5e94209991c7f789e70522a
Kagamma <kagamma.km@gmail.com>
2022-04-20 17:33:09 +0700

    Adds htmlviewer

--- install-packages.sh
+++ install-packages.sh
@@ -1,4 +1,4 @@
 lazbuild --add-package-link src/components/castle-engine/src/vampyre_imaginglib/src/Packages/VampyreImagingPackageExt.lpk
 lazbuild --add-package-link src/components/richmemo/richmemopackage.lpk
-lazbuild --build-ide= --add-package src/components/castle-engine/packages/castle_base.lpk src/components/castle-engine/packages/castle_components.lpk src/components/richmemo/ide/richmemo_design.lpk
+lazbuild --build-ide= --add-package src/components/castle-engine/packages/castle_base.lpk src/components/castle-engine/packages/castle_components.lpk src/components/richmemo/ide/richmemo_design.lpk src/components/HtmlViewer/package/FrameViewer09.lpk
 lazbuild --add-package-link src/components/castle-engine/packages/castle_window.lpk
--- install-submodules.sh
+++ install-submodules.sh
@@ -6,6 +6,7 @@ rm -rf src/components/synalist
 rm -rf src/components/TPocketSphinx
 rm -rf src/components/internettools
 rm -rf src/components/richmemo
+rm -rf src/components/HtmlViewer
 
 git clone --depth 1 https://github.com/Kagamma/satania-buddy-data.git data
 cd src/components
@@ -21,4 +22,5 @@ git clone https://gitlab.com/EugeneLoza/typing-label.git
 svn checkout https://svn.code.sf.net/p/synalist/code/trunk synalist
 git clone --depth 1 https://github.com/Kagamma/internettools.git
 git clone --depth 1 https://github.com/skalogryz/richmemo.git
+git clone --depth 1 https://github.com/Kagamma/HtmlViewer.git
 cd ..
