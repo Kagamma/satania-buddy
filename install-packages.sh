be4f0bd45a86f21943aca8b585cadb642ba492ae
kagamma <kagamma.km@gmail.com>
2022-04-23 02:30:30 +0700

    css package is mandatory

--- install-packages.sh
+++ install-packages.sh
@@ -1,3 +1,3 @@
 lazbuild --add-package-link src/components/castle-engine/src/vampyre_imaginglib/src/Packages/VampyreImagingPackageExt.lpk
-lazbuild --build-ide= --add-package src/components/castle-engine/packages/castle_base.lpk src/components/castle-engine/packages/castle_components.lpk src/components/HtmlViewer/package/FrameViewer09.lpk src/components/KControls/packages/kcontrols/kcontrolsbase.lpk src/components/KControls/packages/kcontrols/kcontrolslaz.lpk
+lazbuild --build-ide= --add-package src/components/castle-engine/packages/castle_base.lpk src/components/castle-engine/packages/castle_components.lpk src/components/HtmlViewer/package/FrameViewer09.lpk src/components/KControls/packages/kcontrols/kcontrolsbase.lpk src/components/KControls/packages/kcontrols/kcontrolslaz.lpk src/components/csscontrols/csspckg_lazarus.lpk
 lazbuild --add-package-link src/components/castle-engine/packages/castle_window.lpk
