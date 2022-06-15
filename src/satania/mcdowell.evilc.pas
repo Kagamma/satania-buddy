fa9f65017633d23b4efe7b64241f748b26e767f4
Kagamma <kagamma.km@gmail.com>
2022-06-15 15:48:58 +0700

    throw -> raise

--- src/satania/mcdowell.evilc.pas
+++ src/satania/mcdowell.evilc.pas
@@ -2138,7 +2138,7 @@ begin
               end ['rsi', 'rdi', 'rax', 'rbx', 'rcx', 'rdx', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'xmm0', 'xmm1', 'xmm2', 'xmm3', 'xmm4', 'xmm5', 'xmm6', 'xmm7'];
             {$endif}
             {$else}
-            throw Exception.Create('Import external function does not support this CPU architecture');
+            raise Exception.Create('Import external function does not support this CPU architecture');
             {$endif} // CPUX86_64
 
             case FuncImportInfo^.Return of
