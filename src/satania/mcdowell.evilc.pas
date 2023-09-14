1c3c3273654fac7c517d5105ac06f713db85fca8
Kagamma <kagamma.km@gmail.com>
2023-09-14 11:57:23 +0700

    Minor

--- src/satania/mcdowell.evilc.pas
+++ src/satania/mcdowell.evilc.pas
@@ -890,7 +890,14 @@ end;
 
 class function TBuiltInFunction.SEBufferLength(const VM: TSEVM; const Args: array of TSEValue): TSEValue;
 begin
-  Result := SESize(Args[0]);
+  case Args[0].Kind of
+    sevkBuffer:
+      begin
+        Result := Length(Args[0].VarBuffer^.Base);
+      end;
+    else
+      Result := 0;
+  end;
 end;
 
 class function TBuiltInFunction.SEBufferGetU8(const VM: TSEVM; const Args: array of TSEValue): TSEValue;
