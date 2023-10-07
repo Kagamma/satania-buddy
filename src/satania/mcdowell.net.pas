8f02984c6d822fe5c323371f30a8d8c4237c74b3
kagamma <7451778+Kagamma@users.noreply.github.com>
2023-10-08 01:43:38 +0700

    http_result_get() can now return data as buffer

--- src/satania/mcdowell.net.pas
+++ src/satania/mcdowell.net.pas
@@ -66,12 +66,35 @@ begin
 end;
 
 procedure TSataniaHttpGetThread.Execute;
+var
+  S: String;
+  I: Integer;
 begin
   try
     try
       HTTP.RequestBody := TRawByteStringStream.Create(FormData.Text);
       HttpResponse.Data := HTTP.Get(URL);
       HttpResponse.Status := HTTP.ResponseStatusCode;
+      HttpResponse.IsBinary := False;
+      for I := 0 to HTTP.ResponseHeaders.Count - 1 do
+      begin
+        if LowerCase(HTTP.ResponseHeaders.Names[I]) = 'content-type' then
+        begin
+          S := LowerCase(HTTP.ResponseHeaders.Values[HTTP.ResponseHeaders.Names[I]]);
+          if S.IndexOf('image/') >= 0 then
+          begin
+            HttpResponse.IsBinary := True;
+          end else
+          begin
+            case S of
+              'application/x-binary',
+              'application/octet-stream':
+                HttpResponse.IsBinary := True;
+            end;
+          end;
+          Break;
+        end;
+      end;
       Synchronize(@SendToHer);
     except
       on E: Exception do
