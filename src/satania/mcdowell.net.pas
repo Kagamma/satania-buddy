c7f2685df910b52ca4405a585e10ad7ebe286ea8
kagamma <7451778+Kagamma@users.noreply.github.com>
2023-10-08 18:48:58 +0700

    Allows to access response headers via http_result_get()

--- src/satania/mcdowell.net.pas
+++ src/satania/mcdowell.net.pas
@@ -14,6 +14,7 @@ type
     procedure SendToHer;
     procedure ResetToDefault;
   public
+    Method: String;
     HTTP: TFPHTTPClient;
     FormData: TStrings;
     HttpResponse: THttpResponseRec;
@@ -73,9 +74,22 @@ begin
   try
     try
       HTTP.RequestBody := TRawByteStringStream.Create(FormData.Text);
-      HttpResponse.Data := HTTP.Get(URL);
+      case Method of
+        'GET':
+          HttpResponse.Data := HTTP.Get(URL);
+        'PUT':
+          HttpResponse.Data := HTTP.Put(URL);
+        'DELETE':
+          HttpResponse.Data := HTTP.Delete(URL);
+        'PATCH':
+          HttpResponse.Data := HTTP.Patch(URL);
+        'OPTIONS':
+          HttpResponse.Data := HTTP.Options(URL);
+      end;
       HttpResponse.Status := HTTP.ResponseStatusCode;
       HttpResponse.IsBinary := False;
+      SetLength(HttpResponse.HeaderKeys, HTTP.ResponseHeaders.Count);    
+      SetLength(HttpResponse.HeaderValues, HTTP.ResponseHeaders.Count);
       for I := 0 to HTTP.ResponseHeaders.Count - 1 do
       begin
         if LowerCase(HTTP.ResponseHeaders.Names[I]) = 'content-type' then
@@ -92,8 +106,9 @@ begin
                 HttpResponse.IsBinary := True;
             end;
           end;
-          Break;
         end;
+        HttpResponse.HeaderKeys[I] := HTTP.ResponseHeaders.Names[I];
+        HttpResponse.HeaderValues[I] := HTTP.ResponseHeaders.Values[HTTP.ResponseHeaders.Names[I]];
       end;
       Synchronize(@SendToHer);
     except
@@ -132,6 +147,8 @@ end;
 procedure TSataniaHttpPostThread.Execute;
 var
   Response: TStringStream;
+  I: Integer;
+  S: String;
 begin
   try
     try
@@ -141,7 +158,6 @@ begin
         try
           HTTP.FileFormPost(URL, FormData, FieldName, FileName, Response);
           HttpResponse.Data := Response.DataString;
-          HttpResponse.Status := HTTP.ResponseStatusCode;
         finally
           Response.Free;
         end;
@@ -149,7 +165,30 @@ begin
       begin
         HTTP.RequestBody := TRawByteStringStream.Create(FormData.Text);
         HttpResponse.Data := HTTP.Post(URL);
-        HttpResponse.Status := HTTP.ResponseStatusCode;
+      end;
+      HttpResponse.Status := HTTP.ResponseStatusCode;
+      HttpResponse.IsBinary := False;
+      SetLength(HttpResponse.HeaderKeys, HTTP.ResponseHeaders.Count);
+      SetLength(HttpResponse.HeaderValues, HTTP.ResponseHeaders.Count);
+      for I := 0 to HTTP.ResponseHeaders.Count - 1 do
+      begin
+        if LowerCase(HTTP.ResponseHeaders.Names[I]) = 'content-type' then
+        begin
+          S := LowerCase(HTTP.ResponseHeaders.Values[HTTP.ResponseHeaders.Names[I]]);
+          if S.IndexOf('image/') >= 0 then
+          begin
+            HttpResponse.IsBinary := True;
+          end else
+          begin
+            case S of
+              'application/x-binary',
+              'application/octet-stream':
+                HttpResponse.IsBinary := True;
+            end;
+          end;
+        end;
+        HttpResponse.HeaderKeys[I] := HTTP.ResponseHeaders.Names[I];
+        HttpResponse.HeaderValues[I] := HTTP.ResponseHeaders.Values[HTTP.ResponseHeaders.Names[I]];
       end;
       Synchronize(@SendToHer);
     except
