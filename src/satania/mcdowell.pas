a70bea6a44d89684fce2c4c3bfa92e3ef71a3086
kagamma <7451778+Kagamma@users.noreply.github.com>
2023-12-09 02:09:44 +0700

    Minor

--- src/satania/mcdowell.pas
+++ src/satania/mcdowell.pas
@@ -466,6 +466,7 @@ begin
   CSAction.Enter;
   try
     Self.Script.IncludePathList.Clear;
+    Self.Script.IncludePathList.Add(GetOSLocalDir);
     Self.Script.IncludePathList.Add('data/scripts/' + Save.Settings.Skin + '/');  
     Self.Script.IncludePathList.Add(GetOSLocalDir + 'data/scripts/' + Save.Settings.Skin + '/');
     case Typ of
