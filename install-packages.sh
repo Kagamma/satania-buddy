084025309f58a4fb98d08d5a560ad3733f937fc2
Kagamma <kagamma.km@gmail.com>
2024-06-03 21:21:03 +0700

    Fixes kcontrols path

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 lazbuild --add-package-link src/components/castle-engine/src/vampyre_imaginglib/src/Packages/VampyreImagingPackageExt.lpk
-lazbuild --build-ide= --add-package src/components/castle-engine/packages/castle_base.lpk src/components/castle-engine/packages/castle_components.lpk src/components/HtmlViewer/package/FrameViewer09.lpk src/components/KControls/packages/kcontrols/kcontrolsbase.lpk src/components/KControls/packages/kcontrols/kcontrolslaz.lpk
+lazbuild --build-ide= --add-package src/components/castle-engine/packages/castle_base.lpk src/components/castle-engine/packages/castle_components.lpk src/components/HtmlViewer/package/FrameViewer09.lpk src/components/KControls/packages/lazarus/kcontrolsbase.lpk src/components/KControls/packages/lazarus/kcontrolslaz.lpk
 lazbuild --add-package-link src/components/castle-engine/packages/castle_window.lpk
